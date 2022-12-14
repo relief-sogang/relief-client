69b4fed1c662cdc5745824227fadb27cd4a8ef02
unibook1283 <uni_book@naver.com>
2022-12-14 23:05:19 +0900

    hotfix

--- components/SideMenu.jsx
+++ components/SideMenu.jsx
@@ -3,6 +3,7 @@ import {TouchableOpacity, View, Text} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import client from '../config/axios';
 import {HomeStyle} from '../styleSheets';
+import {getData} from '../config/asyncStorage';
 
 const SideMenu = ({clickMenu, setClickMenu, moveScreen}) => {
   const [sharingCount, setSharingCount] = useState(0)
